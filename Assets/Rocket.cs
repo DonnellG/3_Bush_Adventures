a9b82c15b6acd5de2c2e99ac70a6ddbb48867392
dgrantham <zankokudonnell@gmail.com>
2020-07-11 13:35:14 -0400

    47_ThrustandRotation

---
+++ Assets/Rocket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody ridgeBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ridgeBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            Quaternion zeroRotation = new Quaternion(0,0,0,0);
+            Vector3 zeroOut = new Vector3(0, 2.5f, 0);
+            print("You reset the position");
+            transform.SetPositionAndRotation(zeroOut, zeroRotation);
+            ridgeBody.AddForce(-Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("you are hiting D");
+            ridgeBody.AddRelativeForce(-Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ridgeBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotate Left");
+            transform.Rotate(Vector3.forward);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotate Right");
+            transform.Rotate(-Vector3.forward);
+        }
+
+
+    }
+
+}
